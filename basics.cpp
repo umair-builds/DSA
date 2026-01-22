bfc8a4751e666f517b975c7b176f05522085ffbe
Umair Sajid <umair454755@gmail.com>
2026-01-23 00:47:18 +0500

    Basics of C++

---
+++ basics.cpp
@@ -0,0 +1,166 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    //if-else
+
+
+    // int x = 10;
+
+    // if(x > 15){
+    //     cout<<"Ok";
+
+    // }
+    // else{
+    //     cout<<x << " not ok";
+    // }
+    // --------------------------------
+
+
+    // //while
+
+    // int y = 1;
+    // while(y <= 5)
+    // {
+    //     cout<< y << " is oh ";
+    //     y++;
+
+    // }
+
+    // //for loop
+
+    // for(int i = 0; i < 10; i++)
+    // {
+    //     cout<<i<< " ";
+    // }
+    // --------------------------------
+
+
+    // *****
+    // *****
+    // *****
+
+    // int c;
+    // int r;
+    // cin>>r>>c;
+
+    // for (int i = 0; i < r; i++)
+    // {
+    //     for (int i = 0; i < c; i++)
+    //     {
+    //         cout<<"*";
+    //     }
+    //     cout<<endl;
+        
+    // }
+
+    // --------------------------------
+
+
+    // 1
+    // 12
+    // 123
+    
+
+    // int r;
+    // cin>>r;
+    // for (int i = 1; i <= r; i++)
+    // {
+    //     for (int j = 1; j <= i; j++)
+    //     {
+    //        cout<<j<<" ";
+    //     }
+    //     cout<<endl;
+        
+    // }
+    // --------------------------------
+
+
+    //Binary to Decimal
+
+    // int n, ans = 0, pow = 1;
+    // cin>>n;
+    // int pre = n;
+
+    // while(n>0)
+    // {
+    //     int d = n % 10;
+    //     ans += d*pow;
+
+    //     n /= 10;
+    //     pow *= 2;
+
+    // }
+
+    // cout<< pre << " --> "<< ans;
+    // --------------------------------
+
+    // decimal to Binary
+
+    // int n;
+    // string ans;
+    // cin>>n;
+    // int pre = n;
+
+    // while(n>0)
+    // {
+    //     int b = n % 2;
+    //     ans = to_string(b)+ans;
+    //     n /=2;
+    // }
+    // cout<< pre << " --> "<< ans;
+    // --------------------------------
+
+    //reverse
+    // int n;
+    // int ans = 0;
+    // cin>>n;
+
+    // while(n>0)
+    // {
+    //     int d = n % 10;
+    //     ans = ans*10 + d;
+    //     n /= 10;
+
+    // }
+    // cout << "reverse is --> "<<ans;
+    // --------------------------------
+
+    // compliment of 2
+    // int n;
+    // cin>>n;
+    // int ans=0;
+    // int pow=1;
+
+
+    // while(n>0){
+    //     int d = !(n%2);
+    //     cout<< d<<endl;
+    //     ans += d*pow;
+    //     pow *= 2;
+    //     n /= 2;
+
+    // }
+    // cout<<ans;
+
+
+    int n;
+cin >> n;
+
+// bitwise logic
+if (n & 1) {
+    cout << "odd";
+} else {
+    cout << "even";
+}
+
+
+
+    
+    
+    
+    
+
+}
